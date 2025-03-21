ef80ce41c7329c3e0a08b2f610b1ee2d83c0d4e1
Sudoix <sudoix.com@gmail.com>
2025-03-21 14:22:56 +0100

    modify maas script

--- maas/maas.sh
+++ maas/maas.sh
@@ -12,12 +12,17 @@ MAAS_VERSION=3.4
 MAAS_DB_USER="maas"
 MAAS_DB_PASS="maaspass"
 MAAS_DB_NAME="maasdb"
+MAAS_URI="http://192.168.178.15:5240/MAAS"
+MAAS_ADMIN_USER="admin"
+MAAS_ADMIN_PASS="admin"
+MAAS_ADMIN_EMAIL="milad@gmail.com"
 POSTGRES_VERSION=16+257build1.1
 POSTGRES_CONFIG_PATH="/etc/postgresql/16/main/pg_hba.conf"
 
+
 # Install Pacakges
 apt update
-apt dist-upgrade -y
+#apt dist-upgrade -y
 snap install --channel="$MAAS_VERSION" maas
 systemctl disable --now systemd-timesyncd
 apt install -y postgresql="$POSTGRES_VERSION"
@@ -28,10 +33,20 @@ sudo -i -u postgres createdb -O "$MAAS_DB_USER" "$MAAS_DB_NAME"
 echo "host    $MAAS_DB_NAME    $MAAS_DB_USER    0/0     md5" >> "$POSTGRES_CONFIG_PATH"
 
 # Initialize maas
-sudo maas init region+rack --database-uri "postgres://$MAAS_DB_USER:$MAAS_DB_PASS@localhost/$MAAS_DB_NAME"
+sudo maas init region+rack --database-uri "postgres://$MAAS_DB_USER:$MAAS_DB_PASS@localhost/$MAAS_DB_NAME" --maas-url "$MAAS_URI"
 
 # Setup admin user and credentials
 
-echo "Please enter your information to create admin user"
-sudo maas createadmin
+echo "Creating admin user"
+sudo maas createadmin --username "$MAAS_ADMIN_USER" --password "$MAAS_ADMIN_PASS" --email "$MAAS_ADMIN_EMAIL"
+if [ $? -eq 0 ]; then
+  echo "Admin user created successfully"
+else
+  echo "Failed to create admin user"
+  exit 1
+fi
+
+echo "reboot the system"
+sync; sync; sync; reboot
+
 
